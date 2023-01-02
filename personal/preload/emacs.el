27467d730177f236499fd8643c8c8f5c3358eaeb
dgabi <dgabi@in-addr.ro>
2023-01-02 19:49:52 +0000

    sane defaults

--- personal/preload/emacs.el
+++ personal/preload/emacs.el
@@ -1,7 +1,15 @@
-(set-face-attribute 'default nil :family "Monaco" :height 150 :weight 'normal)
+(set-face-attribute 'default nil :family "Monaco" :height 160 :weight 'normal)
 
+;;(set-face-background hl-line-face "gray13")
 
 (global-set-key (kbd "C-;") 'find-file)
+(disable-theme 'zenburn)
+(setq prelude-theme 'gruvbox-dark-hard)
+(set-face-background hl-line-face "lightgray")
+(yas-global-mode +1)
 (show-paren-mode 1)
+;; (setq whitespace-line-column 250)
+;; (setq whitespace-style '(face tabs empty trailing lines-tail))
+(scroll-bar-mode -1)
 
-(setq prelude-theme 'adwaita)
+(set-face-background hl-line-face "lightgray")
